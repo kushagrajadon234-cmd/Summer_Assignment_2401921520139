6f0e748d426f0356b85ab5ac706d839aef872904
kushagrajadon234-cmd <kushagrajadon234@gmail.com>
2026-06-08 12:40:10 +0530

    Implement canConstruct function in problem3.cpp

---
+++ Week_2/Day_1/problem3.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char,int> mp;
+        for (auto it : magazine){
+            mp[it]++;
+        }
+        for (auto it : ransomNote){
+            if (mp[it]==0)
+              return  false; 
+            mp[it]--;
+        }
+        return true;
+    }
+};
