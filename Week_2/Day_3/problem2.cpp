ef51d1e3dcbc5212822f345159b2c5bfa549bcb9
kushagrajadon234-cmd <kushagrajadon234@gmail.com>
2026-06-10 15:45:35 +0530

    Implement isSubsequence function in problem2.cpp

---
+++ Week_2/Day_3/problem2.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        if(s.size()>t.size()){
+            return false;
+        }
+        int i=0,j=0;
+        while(i< s.size() && j<t.size()){
+            if(s[i]==t[j]){
+                i++,j++;
+            }
+            else j++;
+        }
+        return i==s.size();
+    }
+};
